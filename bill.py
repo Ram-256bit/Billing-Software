707d53fc15cd96d5841150c27c8615bfee36f341
Ram <72127084+Ram-256bit@users.noreply.github.com>
2021-09-29 20:20:37 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#After completing the whole program, remove the comment on the next 2 lines and move the whole code after that 1 tab space right
+#a = 0
+#while a != 0:
+
 df = pd.read_csv("E:\\New folder(3)\\database.csv")
 no_of_items = int(input('Enter the no of items: '))
 
