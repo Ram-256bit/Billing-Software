11ec2489cc5ff210b9746e1222df79aceba90cc6
Ram <72127084+Ram-256bit@users.noreply.github.com>
2021-09-29 19:48:53 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,8 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("E:\\New folder(3)\\database.csv")
 no_of_items = int(input('Enter the no of items: '))
-
-df = pd.read_csv()
 
 for i in range(no_of_items):
 	print(i)
