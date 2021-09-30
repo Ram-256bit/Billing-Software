b4685b6122d2457b3922ce5d070ca54fcd876a51
Ram <72127084+Ram-256bit@users.noreply.github.com>
2021-10-01 01:14:54 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,27 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv("E:\\New folder(3)\\database.csv",index_col='id')
+df_main = pd.read_csv("E:\\New folder(3)\\database.csv",index_col='id')
 
-#After completing the whole program, remove the comment on the next 2 lines and move the whole code after that 1 tab space right
-#a = 0
-#while a != 0:
-total_cost = 0
-no_of_items = int(input('Enter the no of items: '))
-df1 = pd.DataFrame()
-
-
-for i in range(no_of_items):
-	a = int(input('Enter item code: '))
-	df.loc[a,'no_needed'] = int(input('Number needed: '))
-
-df['cost'] = df['price']*df['no_needed']
-df = df.dropna()
-
-print(df)
-s = df.loc[:,'cost']
-for d in range(s.shape[0]):
-    total_cost = total_cost + df.iloc[d,3]
+#Loop the whole program using while loop
+x = 'y'
+while x == 'y':
+    total_cost = 0
+    df = df_main.copy()
 
 
+    #Getting input:
+    no_of_items = int(input('Enter the no of items: '))
+    for i in range(no_of_items):
+            a = int(input('Enter item code: '))
+            df.loc[a,'no_needed'] = int(input('Number needed:   '))
 
-print('Total cost is: ',total_cost)
+
+    #Modifying the dataframe according to our needs
+    df['cost'] = df['price']*df['no_needed']
+    df = df.dropna()
+    s = df.loc[:,'cost']
+    for d in range(s.shape[0]):
+        total_cost = total_cost + df.iloc[d,3]
+
+
+    #Decorations
+    print('\n\n\n')
+    print('\tAMMAN DEPARTMENTAL STORES')
+    print('\t\tBILL')
+    print('---------------------------------------')
+    print(df)
+    print('---------------------------------------')
+    print('Total cost:\t\t\t ₹',total_cost)
+    print('---------------------------------------')
+
+    #Rerun the program?
+    print('\n\n\n')
+    x = input("Proceed to next bill? (Type 'y' for yes and 'n' for no): ")
+    print('\n\n\n')
