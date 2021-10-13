47bca5b42f5df64100a04b6fa62d8dbc130f64bb
Ram <72127084+Ram-256bit@users.noreply.github.com>
2021-10-13 22:55:03 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -13,26 +13,30 @@ while x == 'y':
     no_of_items = int(input('Enter the no of items: '))
     for i in range(no_of_items):
             a = int(input('Enter item code: '))
-            df.loc[a,'no_needed'] = int(input('Number needed:   '))
+            df.loc[a,'Quantity'] = int(input('Number needed:   '))
 
 
     #Modifying the dataframe according to our needs
-    df['cost'] = df['price']*df['no_needed']
+    df['Cost'] = (df['Price']+(df['Price']*(df['GST %']/100)))*df['Quantity']#GST
     df = df.dropna()
-    s = df.loc[:,'cost']
-    for d in range(s.shape[0]):
-        total_cost = total_cost + df.iloc[d,3]
-
+    #df['Quantity'] = (df.Quantity).astype('int') #--> Print Quantity as int
+    s = df.loc[:,'Cost']
+    for i in range(s.shape[0]):
+        total_cost = total_cost + df.iloc[i,4]
+    total_cost = int(total_cost//1) #Rounds off the value
 
     #Decorations
     print('\n\n\n')
-    print('\tAMMAN DEPARTMENTAL STORES')
-    print('\t\tBILL')
-    print('---------------------------------------')
+    print('\tSELVAPRIYA DEPARTMENTAL STORES')
+    print('\t\t   BILL')
+    print('-----------------------------------------------')
     print(df)
-    print('---------------------------------------')
-    print('Total cost:\t\t\t ₹',total_cost)
-    print('---------------------------------------')
+    print('-----------------------------------------------')
+    print('Total cost:\t\t\t\t ₹',total_cost,sep='')
+    print('-----------------------------------------------')
+    #Printing Balance amount
+    amount = int(input('\nEnter the amount received: ₹ '))
+    print('Balance to be given:       ₹',amount-total_cost)
 
     #Rerun the program?
     print('\n\n\n')
