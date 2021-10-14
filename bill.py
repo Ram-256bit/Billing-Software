5c9ca0a0fe5896aa15e1f4faeae4a8ac8d92a62d
Ram <72127084+Ram-256bit@users.noreply.github.com>
2021-10-14 16:53:20 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,29 +1,47 @@
 import pandas as pd
+import numpy as np
 import os
 df_main = pd.read_csv(os.getcwd()+'\\database.csv',index_col='id')
 
 #Loop the whole program using while loop
-x = 'y'
-while x == 'y':
+x = ''
+while x == '':
+    x = ''                  #for looping the program
     total_cost = 0
     df = df_main.copy()
-
+    df["Quantity"] = np.NaN   #creating a column called Quantity
+    df['Quantity'].fillna(value=0,inplace=True)
 
     #Getting input:
     no_of_items = int(input('Enter the no of items: '))
-    for i in range(no_of_items):
-            a = int(input('Enter item code: '))
+    
+    while no_of_items > 0:
+        a = int(input('Enter item code: '))        
+        if a == 99:
+            no_of_items = no_of_items + 1
+            
+        elif a == 88:
+            no_of_items = no_of_items - 1
+            
+        elif df.loc[a,'Quantity'] == 0:
             df.loc[a,'Quantity'] = int(input('Number needed:   '))
+            no_of_items = no_of_items -1
+            
+        else:
+            df.loc[a,'Quantity'] = int(input('Number needed:   '))+df.loc[a,'Quantity']
+            no_of_items = no_of_items
+            
+        
 
 
     #Modifying the dataframe according to our needs
     df['Cost'] = (df['Price']+(df['Price']*(df['GST %']/100)))*df['Quantity']#GST
-    df = df.dropna()
+    df = df.loc[df['Quantity']!=0,:]
     #df['Quantity'] = (df.Quantity).astype('int') #--> Print Quantity as int
     s = df.loc[:,'Cost']
-    for i in range(s.shape[0]):
+    for i in range(s.shape[0]):                 #Adds the costs
         total_cost = total_cost + df.iloc[i,4]
-    total_cost = int(total_cost//1) #Rounds off the value
+    total_cost = int(total_cost//1)             #Rounds off the value
 
     #Decorations
     print('\n\n\n')
@@ -40,5 +58,5 @@ while x == 'y':
 
     #Rerun the program?
     print('\n\n\n')
-    x = input("Proceed to next bill? (Type 'y' for yes and 'n' for no): ")
+    x = input("Proceed to next bill? (Press Enter for yes and 'n' for no): ")
     print('\n\n\n')
